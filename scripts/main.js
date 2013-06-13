270073845be6724a6771a770d9c59f51246ab72a
ser0n <andre.lilja@gmail.com>
2013-06-13 23:56:48 +0200

    Multiple chunks

--- scripts/main.js
+++ scripts/main.js
@@ -10,4 +10,9 @@ var renderer = new GameRenderer();
 renderer.beginRendering();
 var testChunk = new Chunk(0,0,0);
 renderer.drawChunk(testChunk);
-
+testChunk = new Chunk(16,0,0);
+renderer.drawChunk(testChunk);
+testChunk = new Chunk(16,0,-16);
+renderer.drawChunk(testChunk);
+testChunk = new Chunk(0,0,-16);
+renderer.drawChunk(testChunk);
